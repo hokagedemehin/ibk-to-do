53eefd11dd1070118fc3bd1a071f79a0038748f5
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2020-06-27 22:51:34 +0100

    to do list sample

---
+++ build/precache-manifest.06fdf3392ea657c12e640e2224041942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b581653165ef6d27b0f6873bee22cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4ffd56aacd110fd30b",
+    "url": "/static/css/main.4c76717b.chunk.css"
+  },
+  {
+    "revision": "924f679b4f22c68860f2",
+    "url": "/static/js/2.8a479086.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a479086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4ffd56aacd110fd30b",
+    "url": "/static/js/main.03f6d699.chunk.js"
+  },
+  {
+    "revision": "4c2e562926006afb29f7",
+    "url": "/static/js/runtime-main.630c2b4e.js"
+  }
+]);
